5b11b9437e93ec30a369eb891c2f3695e67dda60
Roger Hardiman <opensource@rjh.org.uk>
2016-08-06 12:06:25 +0100

    Update comments. Add camera address offset - tested with KBD300A Direct P mode

--- pelcod_decoder.js
+++ pelcod_decoder.js
@@ -13,10 +13,15 @@
 /*
  *  SOURCE MATERIAL
  *  Pelco D data sheet (official Pelco document - 1999 Edition)
+ *  Official Pelco KBD300A in Direct D and Direct P modes
+ *
  *  NuOptic D Protocol http://www.nuoptic.com/wp-content/uploads/files/NuOptic_VIS-1000_D-Protocol_Reference.pdf
+ *  CodeProject Pelco D and Pelco P pages http://www.codeproject.com/Articles/8034/Pelco-P-and-D-protocol-implementation-in-C
+ *  ZoneMinder Source Code https://github.com/ZoneMinder/ZoneMinder/tree/master/scripts/ZoneMinder/lib/ZoneMinder/Control 
+ *  iSpyConnect Source Code https://github.com/ispysoftware/iSpy/tree/master/Pelco
+ *  Node-Pelco Source Code https://github.com/Scoup/node-pelcod
  *  CommFront 232 Analizer https://www.commfront.com/pages/pelco-d-protocol-tutorial
  *  CommFront 232 Analizer https://www.commfront.com/pages/pelco-p-protocol-tutorial
- *  CodeProject Pelco D and Pelco P pages http://www.codeproject.com/Articles/8034/Pelco-P-and-D-protocol-implementation-in-C
  * 
  *  Pelco D Commands are 7 bytes long, start with 0xFF and have a 'sum' checksum
  *  Checksum is Sum of bytes 2 to 6 Modulo 256
@@ -59,43 +64,45 @@
  *  |         |  BIT 7 |    BIT 6    |     BIT 5     |   BIT 4     |  BIT 3   |  BIT 2  |  BIT 1  |  BIT 0   |
  *  +---------+--------+-------------+---------------+-------------+----------+---------+---------+----------+
  *  |         |        |             |               |             |          |         |         |          |
- *  |Command 1|Always 0|Camera On/Off|Autoscan On/Off|Camera On/Off|Iris Close|Iris Open|Focus Near|Focus Far|
+ *  |Command 1|Unknown |Unknown      |Unknown        |Unknown      |Iris Close|Iris Open|Focus Near|Focus Far|
  *  |         |        |             |               |             |          |         |         |          |
- *  |Command 2|Always 0|Zoom Wide    |Zoom Tele      |Tilt Down    |Tilt Up   |Pan Left |Pan Right|Always 0  |
+ *  |Command 2|Unknown |Zoom Wide    |Zoom Tele      |Tilt Down    |Tilt Up   |Pan Left |Pan Right|Always 0  |
  *  +---------+--------+-------------+---------------+-------------+----------+---------+---------+----------+
- * The Pelco P table comes from various web sites and not from any formal documents
+ * The Pelco P table comes from various web sites and not from any formal documents and so the true meanings of
+ * Command 1 bits 7,6,5 and 4 and Command 2 bit 7 is not properly known. One site lists some of these as
+ * Camera On/Off and Auto Scan bits but cannot verify this.
  *
  *
  * EXTENDED COMMANDS
- * Bit 0 of Command 2 is set to '1' for extended commands (giving Command 2 an 'odd' numerical value)
- * Byte 4 contains the extended command. Bytes 5 and 6 are used for data values. Byte 3 is used for additional settings
- * There are a large number of extended commands. This code processes the common commands
- *  +--------------------------------+--------+--------+---------------------+-------------+----+
- *  |                                | BYTE 3 | BYTE 4 |       BYTE 5        |   BYTE 6    |D/P |
- *  |                                | Cmd 1  | Cmd 2  |       Data 1        |   Data 2    |    |
- *  +--------------------------------+--------+--------+---------------------+-------------+----+
- *  |                                |        |        |                     |             |    |
- *  | Set Preset                     | 00     | 03     | 00                  | value       |Both|
- *  |                                |        |        |                     |             |    |
- *  | Clear Preset                   | 00     | 05     | 00                  | value       |Both|
- *  |                                |        |        |                     |             |    |
- *  | Go To Preset                   | 00     | 07     | 00                  | value       |Both|
- *  |   Flip (180deg about)          | 00     | 07     | 00                  | 21          |    |
- *  |   Go To Zero Pan               | 00     | 07     | 00                  | 22          |    |
- *  |                                |        |        |                     |             |    |
- *  | Set Auxiliary                  | 00     | 09     | 00                  | value       |D   |
- *  |                                |        |        |                     |             |    |
- *  | Clear Auxiliary                | 00     | 0B     | 00                  | value       |D   |
- *  |                                |        |        |                     |             |    |
- *  | Set Pattern Start              | 00     | 1F     | 00                  | value       |Both|
- *  |                                |        |        |                     |             |    |
- *  | Set Pattern Stop               | 00     | 21     | 00                  | value       |Both|
- *  |                                |        |        |                     |             |    |
- *  | Run Pattern                    | 00     | 23     | 00                  | value       |Both|
- *  |                                |        |        |                     |             |    |
- *  | Set Zoom Speed                 | 00     | 25     | 00                  | value (0-3) |Both|
- *  |                                |        |        |                     |             |    |
- *  +--------------------------------+--------+--------+---------------------+-------------+----+
+ * Bit 0 of Command 2 is set to '1' for extended commands.
+ * Bytes 3,4,5 and 6 contain the extended command and any values
+ * There are a large number of extended commands. This code processes the common commands.
+ *  +--------------------------------+--------+--------+--------+-------------+----+
+ *  |                                | BYTE 3 | BYTE 4 | BYTE 5 | BYTE 6      |D/P |
+ *  |                                | Cmd 1  | Cmd 2  | Data 1 | Data 2      |    |
+ *  +--------------------------------+--------+--------+--------+-------------+----+
+ *  |                                |        |        |        |             |    |
+ *  | Set Preset                     | 00     | 03     | 00     | value       |Both|
+ *  |                                |        |        |        |             |    |
+ *  | Clear Preset                   | 00     | 05     | 00     | value       |Both|
+ *  |                                |        |        |        |             |    |
+ *  | Go To Preset                   | 00     | 07     | 00     | value       |Both|
+ *  |   Flip (180deg about)          | 00     | 07     | 00     | 21          |    |
+ *  |   Go To Zero Pan               | 00     | 07     | 00     | 22          |    |
+ *  |                                |        |        |        |             |    |
+ *  | Set Auxiliary                  | 00     | 09     | 00     | value       |Both|
+ *  |                                |        |        |        |             |    |
+ *  | Clear Auxiliary                | 00     | 0B     | 00     | value       |Both|
+ *  |                                |        |        |        |             |    |
+ *  | Set Pattern Start              | 00     | 1F     | 00     | value       |Both|
+ *  |                                |        |        |        |             |    |
+ *  | Set Pattern Stop               | 00     | 21     | 00     | value       |Both|
+ *  |                                |        |        |        |             |    |
+ *  | Run Pattern                    | 00     | 23     | 00     | value       |Both|
+ *  |                                |        |        |        |             |    |
+ *  | Set Zoom Speed                 | 00     | 25     | 00     | value (0-3) |Both|
+ *  |                                |        |        |        |             |    |
+ *  +--------------------------------+--------+--------+--------+-------------+----+
  *
  */
 
@@ -226,7 +233,7 @@ PelcoD_Decoder.prototype.decode = function(pelco_command_buffer) {
     }
     if (pelco_p) {
         //var sync      = pelco_command_buffer[0];
-        var camera_id = pelco_command_buffer[1];
+        var camera_id = pelco_command_buffer[1] + 1; // Pelco P sends Cam1 as 0x00
         var command_1 = pelco_command_buffer[2];
         var command_2 = pelco_command_buffer[3];
         var data_1 = pelco_command_buffer[4];
