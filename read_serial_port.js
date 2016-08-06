34ff37a975219e3b006cf93cecae5a7a773dad61
Roger Hardiman <opensource@rjh.org.uk>
2016-08-06 12:06:06 +0100

    Add notes on Pelco P serial port baud rate

--- read_serial_port.js
+++ read_serial_port.js
@@ -1,8 +1,16 @@
 /*
  *
- * Read and decode Pelco D CCTV commands from a Serial Port / COM Port (using node-serialport)
+ * Read and decode Pelco D and Pelco P CCTV commands from a Serial Port / COM Port (using node-serialport)
  * Copyright 2016 Roger Hardiman
  *
+ * You will need to set the SERIAL_PORT and the BAUD_RATE
+ * Note that the Serial Port varies between operating systems
+ * E.g.   COM1 on Windows
+ * E.g.   /dev/ttyUSB0 on Linux
+ * E.g.   /dev/cu.usbserial-141 on Mac
+ *
+ * The Baud Rate for Pelco D is often 2400 8-N-1
+ * The Baud Rate for Pelco P is often 4800 8-N-1
  */
 
 // External Dependencies
@@ -10,7 +18,7 @@ var SerialPort = require('serialport').SerialPort;
 var PelcoD_Decoder = require('./pelcod_decoder');
 
 // User Settings
-var SERIAL_PORT = 'COM2';    // or /dev/ttyUSB0 on Linux
+var SERIAL_PORT = '/dev/ttyUSB0';
 var BAUD_RATE = 2400;
 
 // Globals
