8e9e8c3553d9143bd1f8dd9c9afaed6a8aba8cd0
Roger Hardiman <opensource@rjh.org.uk>
2016-08-22 20:32:06 +0100

    Add Bosch/Philips protocol decoding using Bosch OSRD manual

--- pelcod_decoder.js
+++ pelcod_decoder.js
@@ -119,12 +119,20 @@ function PelcoD_Decoder() {
 
     // Number of bytes in the current Buffer
     this.pelco_p_command_index = 0;
+
+    // A Buffer used for byte Bosch/Philips BiPhase
+    // Max length is 128 as length is bits 0 to 6 of header
+    this.bosch_command_buffer = new Buffer(128);
+
+    // Number of bytes in the current Buffer
+    this.bosch_command_index = 0;
+
 }
 
 
 PelcoD_Decoder.prototype.processBuffer = function(new_data_buffer) {
 
-    // console.log('received ' + this.bytes_to_string(new_data_buffer) );
+    // console.log('received ' + this.bytes_to_string(new_data_buffer,new_data_buffer.length) );
 
     // process each byte from new_data_buffer in turn
 
@@ -161,6 +169,18 @@ PelcoD_Decoder.prototype.processBuffer = function(new_data_buffer) {
             this.pelco_p_command_buffer[this.pelco_p_command_buffer.length-1] = new_byte;
         }
 
+        // Add to Bosch 8 byte buffer
+	if (new_byte & 0x80) {
+console.log('BOSCH HEADER');
+	    // MSB set to 1. This marks the start of a Bosch command so reset buffer counter
+	    this.bosch_command_index = 0;
+	}
+        if (this.bosch_command_index < this.bosch_command_buffer.length) {
+            // Add the new_byte to the end of the bosch_command_buffer
+            this.bosch_command_buffer[this.bosch_command_index] = new_byte;
+            this.bosch_command_index++;
+        }
+
 
         // Pelco D Test. Check if we have 7 bytes with byte 0 = 0xFF and with a valid SUM checksum
         if (this.pelco_command_index === 7 && this.pelco_command_buffer[0] === 0xFF
@@ -177,6 +197,16 @@ PelcoD_Decoder.prototype.processBuffer = function(new_data_buffer) {
             // Looks like we have a Pelco command. Try and process it
             this.decode(this.pelco_p_command_buffer);
             this.pelco_p_command_index = 0; // empty the buffer
+        }
+
+        // Bosch Test. First byte has MSB of 1. First byte is the message size (excluding the checksum)
+        var bosch_len = this.bosch_command_buffer[0] & 0x7F;
+        if ((this.bosch_command_buffer[0] & 0x80)
+                                             && this.bosch_command_index == (bosch_len + 1)
+                                             && this.checksum_bosch_valid(this.bosch_command_buffer, this.bosch_command_index)) {
+            // Looks like we have a Bosch command. Try and process it
+            this.decode_bosch(this.bosch_command_buffer);
+            this.bosch_command_index = 0; // empty the buffer
         }
     }
 };
@@ -209,6 +239,19 @@ PelcoD_Decoder.prototype.checksum_p_valid = function(buffer) {
     }
 };
 
+PelcoD_Decoder.prototype.checksum_bosch_valid = function(buffer,message_length) {
+    var total = 0;
+    for (var x = 0; x < (message_length - 1); x++) {
+        total += buffer[x];
+    }
+    var computed_checksum = total & 0x7F; // MSB set to 1 is reserved for first byte.
+    // Check if computed_checksum matches the last byte in the buffer
+    if (computed_checksum === buffer[message_length - 1]) {
+        return true;
+    } else {
+        return false;
+    }
+};
 
 PelcoD_Decoder.prototype.decode = function(pelco_command_buffer) {
 
@@ -355,12 +398,169 @@ PelcoD_Decoder.prototype.decode = function(pelco_command_buffer) {
         }
 
     }
-    console.log(this.bytes_to_string(pelco_command_buffer) + ' ' + msg_string);
+    console.log(this.bytes_to_string(pelco_command_buffer, pelco_command_buffer.length) + ' ' + msg_string);
 };
 
-PelcoD_Decoder.prototype.bytes_to_string = function(buffer) {
+PelcoD_Decoder.prototype.decode_bosch = function(bosch_command_buffer) {
+
+    // Note Bosch is 9600 8-N-1
+
+    var msg_string ='';
+
+    msg_string += 'Bosch ';
+
+    // TO DO - Add variable length message support
+    var length      = bosch_command_buffer[0] & 0x7F;
+    var high_order_address = bosch_command_buffer[1];
+    var low_order_address = bosch_command_buffer[2];
+    var op_code = bosch_command_buffer[3];
+    //var data_byte_1 = bosch_command_buffer[4];
+    //var data_byte_2 = bosch_command_buffer[5];
+    //var data_byte_3 = bosch_command_buffer[6];
+    //var data_byte_X = bosch_command_buffer[xxx];
+    //var checksum = bosch_command_buffer[the last byte]
+
+    var camera_id = (high_order_address << 7) + low_order_address + 1;
+
+    msg_string += 'Camera ' + camera_id + ' ';
+    
+    // Process Op Code
+    if (op_code == 0x02) {
+        msg_string += 'Start/Stop Fixed Speed PTZ, Focus and Iris';
+    }
+    else if (op_code == 0x03) {
+        msg_string += 'Fixed Speed PTZ for a specified period';
+    }
+    else if (op_code == 0x04) {
+        msg_string += 'Repetitive Fixed Speed PTZ';
+    }
+    else if (op_code == 0x05) {
+        msg_string += 'Start/Stop Variable Speed PTZ = ';
+        // 3 data bytes used with this Op Code
+        var data_1 = bosch_command_buffer[4];
+        var data_2 = bosch_command_buffer[5];
+        var data_3 = bosch_command_buffer[6];
+        var zoom_speed = (data_1 >> 4) & 0x07;
+        var tilt_speed = (data_1 >> 0) & 0x0F;
+        var pan_speed  = (data_2 >> 3) & 0x0F;
+        var iris_open  = (data_2 >> 2) & 0x01;
+        var iris_close = (data_2 >> 1) & 0x01;
+        var focus_far  = (data_2 >> 0) & 0x01;
+        var focus_near = (data_3 >> 6) & 0x01;
+        var zoom_in    = (data_3 >> 5) & 0x01;
+        var zoom_out   = (data_3 >> 4) & 0x01;
+        var up    = (data_3 >> 3) & 0x01;
+        var down  = (data_3 >> 2) & 0x01;
+        var left  = (data_3 >> 1) & 0x01;
+        var right = (data_3 >> 0) & 0x01;
+
+
+        if (left === 0 && right === 0) {
+            msg_string += '[pan stop     ]';
+        } else if (left === 1 && right === 0) {
+            msg_string += '[PAN LEFT ('+pan_speed+')]';
+        } else if (left === 0 && right === 1) {
+            msg_string += '[PAN RIGHT('+pan_speed+')]';
+        } else { // left === 1 && right === 1)
+            msg_string += '[PAN ???? ('+pan_speed+')]';
+        }
+
+        if (up === 0 && down === 0) {
+            msg_string += '[tilt stop    ]';
+        } else if (up === 1 && down === 0) {
+            msg_string += '[TILT UP  ('+tilt_speed+')]';
+        } else if (up === 0 && down === 1) {
+            msg_string += '[TILT DOWN('+tilt_speed+')]';
+        } else { // (up === 1 && down === 1)
+            msg_string += '[TILT ????('+tilt_speed+')]';
+        }
+
+        if (zoom_in === 0 && zoom_out === 0) {
+            msg_string += '[zoom stop]';
+        } else if (zoom_in === 1 && zoom_out === 0) {
+            msg_string += '[ZOOM IN('+zoom_speed+')]';
+        } else if (zoom_in === 0 && zoom_out === 1) {
+            msg_string += '[ZOOM OUT('+zoom_speed+')]';
+        } else { // (zoom_in === 1 && zoom_out === 1)
+            msg_string += '[ZOOM ????]';
+        }
+
+        if (iris_open === 0 && iris_close === 0) {
+            msg_string += '[iris stop ]';
+        } else if (iris_open === 1 && iris_close === 0) {
+            msg_string += '[IRIS OPEN ]';
+        } else if (iris_open === 0 && iris_close === 1) {
+            msg_string += '[IRIS CLOSE]';
+        } else { // (iris_open === 1 && iris_close === 1)
+            msg_string += '[IRIS ???? ]';
+        }
+
+        if (focus_near === 0 && focus_far === 0) {
+            msg_string += '[focus stop]';
+        } else if (focus_near === 1 && focus_far === 0) {
+            msg_string += '[FOCUS NEAR]';
+        } else if (focus_near === 0 && focus_far === 1) {
+            msg_string += '[FOCUS FAR ]';
+        } else { // (focus_near === 1 && focus_far === 1)
+            msg_string += '[FOCUS ????]';
+        }
+    }
+    else if (op_code == 0x06) {
+        msg_string += 'Repetitive Fixed speed Zoom, Focus and Iris';
+    }
+    else if (op_code == 0x07) {
+        msg_string += 'Auxiliary On/Off and Preposition Set/Shot = ';
+        // 2 data bytes used with this Op Code
+        var data_1 = bosch_command_buffer[4];
+        var data_2 = bosch_command_buffer[5];
+        var function_code = data_1 & 0x0F;
+        var data = (data_1 >> 4) + data_2;
+        if (function_code == 1) msg_string += 'Aux On ' + data;
+        else if (function_code == 2) msg_string += 'Aux Off ' + data;
+        else if (function_code == 4) msg_string += 'Pre-position SET ' + data;
+        else if (function_code == 5) msg_string += 'Pre-position SHOT ' + data;
+        else if (function_code == 8) msg_string += 'Cancel Latching Aux ' + data;
+        else if (function_code == 9) msg_string += 'Latching Aux On ' + data;
+        else if (function_code == 10) msg_string += 'Latching Aux Off ' + data;
+	else msg_string += 'unknown aux or pre-position command ' + function_code + ' with value ' + data;
+    }
+    else if (op_code == 0x08) {
+        msg_string += 'Repetitive Variable-speed PTZ, Focus and Iris';
+    }
+    else if (op_code == 0x09) {
+        msg_string += 'Fine Speed PTZ';
+    }
+    else if (op_code == 0x0A) {
+        msg_string += 'Position Report and Replay / Position Commands';
+    }
+    else if (op_code == 0x0C) {
+        msg_string += 'Ping Command';
+    }
+    else if (op_code == 0x0F) {
+        msg_string += 'Information Requested / Reply';
+    }
+    else if (op_code == 0x10) {
+        msg_string += 'Title set';
+    }
+    else if (op_code == 0x12) {
+        msg_string += 'Auxiliary Commands with Data';
+    }
+    else if (op_code == 0x13) {
+        msg_string += 'Set Position / Get Position';
+    }
+    else if (op_code == 0x14) {
+        msg_string += 'BiCom message';
+    }
+    else {
+        msg_string += 'Unknown Op Code ' + op_code;
+    }
+
+    console.log(this.bytes_to_string(bosch_command_buffer,length+1) + ' ' + msg_string);
+};
+
+PelcoD_Decoder.prototype.bytes_to_string = function(buffer, length) {
     var byte_string = '';
-    for (var i = 0; i < buffer.length; i++) {
+    for (var i = 0; i < length; i++) {
         byte_string += '[' + this.DecToHexPad(buffer[i],2) + ']';
     }
     return byte_string;
